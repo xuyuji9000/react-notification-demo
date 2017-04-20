7b8006d47b4535737535caa49a5d0f94fb2b4d2e
Karl Xu <karl.yogi.xu@gmail.com>
2017-04-20 20:36:55 +0800

    add notification

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Test from './components/Test'
+import createReactClass from 'create-react-class'
+import NotificationSystem from 'react-notification-system'
 
+const MyComponent = createReactClass({
+    _notificationSystem: null,
+
+    _addNotification: function(event) {
+        event.preventDefault()
+
+        this._notificationSystem.addNotification({
+            message: 'Notification message',
+            level: 'success'
+        })
+    },
+
+    componentDidMount: function() {
+        this._notificationSystem = this.refs.notificationSystem
+    },
+
+    render: function() {
+        return (
+            <div>
+                <button onClick={this._addNotification}>Add notification</button>
+                <NotificationSystem ref='notificationSystem'></NotificationSystem>
+            </div>
+        )
+    }
+})
 
 
 render(
-    <Test></Test>,
+    <MyComponent></MyComponent>,
     document.getElementById('root')
 )
